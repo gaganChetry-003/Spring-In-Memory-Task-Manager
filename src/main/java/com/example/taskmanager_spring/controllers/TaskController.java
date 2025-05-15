886390919a37f4e9d7fda44f35eaa2e59837783b
gagan_003 <gaganchetry0702@gmail.com>
2025-05-15 23:21:34 +0530

    Made the post and get Task API's

---
+++ src/main/java/com/example/taskmanager_spring/controllers/TaskController.java
@@ -0,0 +1,49 @@
+package com.example.taskmanager_spring.controllers;
+
+
+import com.example.taskmanager_spring.dto.CreateTaskDTO;
+import com.example.taskmanager_spring.entities.TaskEntity;
+import com.example.taskmanager_spring.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ArrayList<TaskEntity>> getTasks() {
+        var tasks=taskService.getTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskEntity> getTaskById(int id) {
+        TaskEntity task=taskService.getTaskById(id);
+        if(task==null)
+        {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(task);
+
+    }
+@PostMapping("")
+
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body){
+
+       var task= taskService.addTask(body.getTitle(),body.getDescription(),body.getDeadline());
+        return ResponseEntity.ok(task);
+
+}
+
+
+
+
+}
